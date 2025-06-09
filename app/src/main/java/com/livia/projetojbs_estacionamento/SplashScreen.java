8e477ff58afd0c974431bc428ff9e95caa9718c7
Lívia Izidório <liviaizidorio.li@gmail.com>
2025-06-09 18:19:08 -0300

    Criando splash screen.

---
+++ app/src/main/java/com/livia/projetojbs_estacionamento/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.livia.projetojbs_estacionamento;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        Glide.with(this)
+                .asGif()
+                .load(R.drawable.video)
+                .into(imageView);
+        new Handler().postDelayed(this::abrirTela, 9000);
+    }
+    private void abrirTela(){
+        Intent rota= new Intent (this,Cadastro.class);
+        startActivity(rota);
+        finish();
+    }
+}
